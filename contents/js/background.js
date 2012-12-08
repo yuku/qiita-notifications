92ec085dd29c58b8e4094cdee2a8e552e1a306e2
Yuku Takahashi <taka84u9@gmail.com>
2012-12-08 12:07:03 +0900

    Setup google analytics

--- contents/js/background.js
+++ contents/js/background.js
@@ -214,19 +214,3 @@
   };
 
 })(jQuery, CrO3);
-
-
-
-// Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-30142578-1']);
-_gaq.push(['_trackPageview']);
-(function () {
-  /*jshint strict: false */
-  var ga = document.createElement('script');
-  ga.type = 'text/javascript';
-  ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
-})();
