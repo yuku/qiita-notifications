3f7010c385b7ce1fc01ceac85fc8a76010a24f7e
Yuku Takahashi <taka84u9@gmail.com>
2013-07-03 18:49:19 +0900

    Revert "Unsupport lgtm notifications"
    This reverts commit 7741072dcdb6c414a96b8a2744810ddf0ad5f499.

--- contents/js/background.js
+++ contents/js/background.js
@@ -167,6 +167,10 @@
                   title = users + 'があなたのコメントした投稿にコメントしました';
                   body = datum.short_title;
                   break;
+                case 'lgtm':
+                  title = users + 'があなたの投稿にLGTMしました';
+                  body = datum.short_title;
+                  break;
                 case 'item_mention':
                   title = users + 'があなたにメンションしました';
                   body = datum.short_title;
